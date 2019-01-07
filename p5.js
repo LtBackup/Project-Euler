7eecfbf477688eea07fa759cde51b7117d982316
James Pratt <jamespratt6@gmail.com>
2019-01-07 01:40:08 -0600

    first stab at p5

---
+++ p5.js
@@ -0,0 +1,17 @@
+const productDivisibleUpTo20 = function () {
+    let starterFactor = 1 * 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19;
+    let testValue = starterFactor;
+    let result = 0;
+    while (result < Math.pow(2, 53)){
+        if(testValue % 20 === 0 && testValue % 19 === 0 && testValue % 18 === 0 && testValue % 16 === 0 && testValue % 15 === 0 && testValue % 14 === 0 && testValue %13 === 0 && testValue % 12 === 0 && testValue % 11 === 0){
+            return testValue;
+        }
+        else{
+            testValue += starterFactor;
+        }
+    }
+
+console.log(productDivisibleUpTo20());
+
+    //only need to multiply primes since everything else is divisible by something smaller than it.
+    //add primes up until one works
